2bdaca19711b84a7a37be720d45f12ed06f2e41e
sammia28 <sammia.zubizarreta@gmail.com>
2016-08-25 17:29:19 -0500

    ejercicio 7

---
+++ app.js
@@ -0,0 +1,10 @@
+var num = prompt("Ingresa número");
+if(num % 2 !=0){
+    window.alert("odd!");    
+}else if(num <=5){
+    window.alert("Good!")
+}else if(num >=6 && num <=20){
+    window.alert("Great!")
+}else{
+    window.alert("perfect!")
+}
